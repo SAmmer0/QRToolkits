9a52e3e3decc2bcbd817575bb3bf20776a03e823
c-PC\c <howardleeh@gmail.com>
2018-04-26 11:23:16 +0800

    对暴露接口进行整理

--- pitdata/__init__.py
+++ pitdata/__init__.py
@@ -8,3 +8,8 @@ Created: 2018/4/17
 
 Point in time data manager
 """
+from pitdata.utils import DataDescription
+from pitdata.tools import PITDataCache, pitcache_getter, delete_computing_file, move_computing_file
+from pitdata.query import query, query_group, list_data, show_all_data
+from pitdata.const import DataType
+from pitdata.updater.operator import update_all
