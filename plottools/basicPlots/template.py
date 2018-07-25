8e96b39fea14aa974a0d3d71e6f6e5b688701270
c-PC\c <howardleeh@gmail.com>
2018-07-25 16:01:20 +0800

    添加日期轴绘图模板，并通过测试

--- plottools/basicPlots/template.py
+++ plottools/basicPlots/template.py
@@ -10,6 +10,7 @@ import pdb
 import re
 
 from plottools.basicPlots.generalplots import PlotFramework
+from plottools.basicPlots.components import AxisComponent, date_ticker
 
 # --------------------------------------------------------------------------------------------------
 # 模板
@@ -47,7 +48,6 @@ class PlotTemplateBase(object):
         if item.startswith('setc_'):
             cls_name = re.match('setc_([_0-9a-zA-Z]*)', item).groups()[0]
             valid_settings = ['title', 'legend', 'annotate', 'xaxis', 'yaxis', 'secondary_yaxis', 'others']
-            print(cls_name)
             if cls_name not in valid_settings:
                 raise AttributeError
             def set_handler(handler_obj):
@@ -77,3 +77,56 @@ class PlotTemplateBase(object):
         res_axes = plot_framework.plot()
         return res_axes
 
+class DateXAxisPlot(PlotTemplateBase):
+    '''
+    x轴为时间时的绘图处理函数
+
+    Parameter
+    ---------
+    main_plot_creator: function(*data, **kwargs)->func(axes)
+        主绘图函数
+    date_locator: function(date)->boolean
+        计算当前tick是否为刻度的定位函数
+    date_formatter: string, default '%Y-%m-%d'
+        使用date_fromatter.format(date)来获取对应刻度的标签
+    main_plot_kwargs: dictionary, default None
+        传入到主绘图组件中的其他参数
+    xaxis_kwargs: dictionary, default None
+        传入到AxisComponent中的关键字参数
+    '''
+    def __init__(self, main_plot_creator, date_locator, date_formatter='{:%Y-%m-%d}',
+                 add_header=True, add_tail=False, main_plot_kwargs=None, xaxis_kwargs=None):
+        if main_plot_kwargs is None:
+            self._main_plot_kwargs = {}
+        else:
+            self._main_plot_kwargs = main_plot_kwargs
+        if xaxis_kwargs is None:
+            self._xaxis_kwargs = {}
+        else:
+            self._xaxis_kwargs = xaxis_kwargs
+        super().__init__(main_plot_creator, **self._main_plot_kwargs)
+        self._ticker_kwargs = {'locator': date_locator, 'formatter': date_formatter,
+                               'add_header': add_header, 'add_tail': add_tail}
+
+    def plot(self, axes, *data, axis_dates):
+        '''
+        执行绘图
+
+        Parameter
+        ---------
+        axes: WrappedTwinAxes
+            子图
+        data: tuple
+            绘图的相关数据
+        axis_dates: iterable, keyword only
+            日期轴数据
+
+        Return
+        ------
+        axes: WrappedTwinAxes
+            完成绘图后的子图
+        '''
+        major_ticks_pos, major_ticks_labels = date_ticker(axis_dates, **self._ticker_kwargs)
+        ac = AxisComponent(major_ticks_pos, major_ticks_labels, **self._xaxis_kwargs)
+        self.setc_xaxis(ac)
+        super().plot(axes, *data)
--- plottools/test/template_test.py
+++ plottools/test/template_test.py
@@ -1,1 +1,33 @@
+#!/usr/bin/env python
+# -*- coding:utf-8
+"""
+Author:  Hao Li
+Email: howardleeh@gmail.com
+Github: https://github.com/SAmmer0
+Created: 2018/7/25
+"""
 
+import matplotlib.pyplot as plt
+
+from pitdata import query_group
+from tdtools import get_calendar
+from plottools.basicPlots.template import DateXAxisPlot
+from plottools.basicPlots.components import KLinePlot
+
+def main_tester(test_func):
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    test_func(ax)
+
+def datexaxis_test(ax):
+    data = query_group(['OPEN', 'CLOSE', 'HIGH', 'LOW'], '2018-01-01', '2018-07-25')
+    data = data.iloc[:, 0].unstack()
+    def creator(*data, **kwargs):
+        return KLinePlot(*data, **kwargs)
+    calendar = get_calendar('stock.sse')
+    plotter = DateXAxisPlot(creator, lambda x: calendar.is_cycle_target(x, 'MONTHLY', 'LAST'),
+                            xaxis_kwargs={'ticklabel_kwargs': {'rotation': 30}})
+    plotter.plot(ax, data['OPEN'], data['CLOSE'], data['HIGH'], data['LOW'], axis_dates=data.index)
+
+if __name__ == '__main__':
+    main_tester(datexaxis_test)
