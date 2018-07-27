73837aabfe3a37318bc218039e71db14daf10ec2
c-PC\c <howardleeh@gmail.com>
2018-07-27 11:14:34 +0800

    添加模块API

--- plottools/__init__.py
+++ plottools/__init__.py
@@ -8,3 +8,7 @@ Created: 2018/7/11
 
 基本绘图模块，用于提供基本的绘图工具
 """
+from plottools.layout import GeneralNLineLayout, GridLayoutBase, NKLayout, NLineLayout
+from plottools.plot_helper.font_helper import chinese_font_context
+from plottools.plot_helper.kline_helper import candlestick2_ohlc
+from plottools.plot_helper.tick_helper import date_ticker
